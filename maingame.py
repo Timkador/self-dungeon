3ae62e21e1be2f110644853f65ee499d65c9d0a9
Timkador <104220939+Timkador@users.noreply.github.com>
2022-04-23 16:57:30 +0700

    Add files via upload

--- maingame.py
+++ maingame.py
@@ -1,28 +1,24 @@
 import arcade
-import _thread
-from random import randint, choice 
-import time
-
+from random import randint, choice
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Self-dungeon"
 act = 0
 tick = 100
-
+delta_stockpile = 0.0
 
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color((78,5,27))
         self.player = None
 
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
         # Create your sprites and sprite lists here
-        self.rectangles = []
         self.player = {
-            'x': 0,
-            'y': 0,
+            'x': 80,
+            'y': 80,
             'width': 20,
             'height': 20,
             'color': (22,222,22),
@@ -33,33 +29,32 @@ class MyGame(arcade.Window):
         self.clear()
 
 
-    def grid_draw(self, x, y, sqwidth, sqheight, versquares, horsquares, rgbcolor, linesize=1):
+    def grid_draw(self, x, y, sqwidth, sqheight, horsquares, versquares, rgbcolor, linesize=1):
         for i in range(versquares):
             for j in range(horsquares):
                 arcade.draw_rectangle_filled(x+(sqwidth*j), y+(sqheight*i), sqwidth-linesize, sqwidth-linesize, rgbcolor)
 
     def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        pass
-
+        global delta_stockpile
+        delta_stockpile += delta_time
+        if delta_stockpile > 0.5:
+            ch = choice(['x-1', 'x1', 'y-1', 'y+1'])
+            self.player[ch[0]] = self.player[ch[0]] + int(ch[1:])*self.player['mov_dis']
+            if self.player[ch[0]] < 0:
+                self.player[ch[0]] += self.player['mov_dis']
+            elif ch[0] == 'x' and self.player[ch[0]] > SCREEN_WIDTH:
+                self.player[ch[0]] -= self.player['mov_dis']
+            elif ch[0] == 'y' and self.player[ch[0]] > SCREEN_HEIGHT:
+                self.player[ch[0]] -= self.player['mov_dis']
+            arcade.start_render()
+            self.grid_draw(0,0,40,40,21,16,(156,10,54), 2)
+            arcade.draw_rectangle_filled(self.player['x'], self.player['y'], self.player['width'], self.player['height'], self.player['color'])
+            arcade.finish_render()
+            delta_stockpile = 0.0
+        
+    
     def on_key_release(self, key, modifiers):
-        if key == arcade.key.UP:
-            self.player['y'] += 40
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player['y'] -= 40
-        elif key == arcade.key.LEFT or key == arcade.key.A:
-            self.player['x'] -= 40
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player['x'] += 40
-        print(self.player['x'],self.player['y'])
-        arcade.start_render()
-        self.grid_draw(0,0,40,40,20,15,(156,10,54), 2)
-        arcade.draw_rectangle_filled(self.player['x'], self.player['y'], self.player['width'], self.player['height'], self.player['color'])
-        arcade.finish_render()
+        pass
 
 
 
