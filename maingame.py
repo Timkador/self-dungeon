c90cc5cd0365df610cd3f3677518d62d5604449b
Timkador <104220939+Timkador@users.noreply.github.com>
2022-04-30 03:00:04 +0700

    Add files via upload

--- maingame.py
+++ maingame.py
@@ -5,14 +5,12 @@ SCREEN_HEIGHT = 800
 SCREEN_TITLE = "Self-dungeon"
 delta_stockpile = 0.0
 
-#Color of the game is basaed of a single background color, because I think that's a cool idea. If that won't work, I'll just use it for convinient color assignment.
 class Color():
     def __init__(self, background):
         self.color = {
             'background': background
         }
         self.colmanual = {}
-    #Colormanual is formatted into a list of 3 multipliers that are multiplied by to get a correct color value
     def manual_edit(self, instructions, name, scolor):
         self.colmanual[name] = [instructions, scolor]
         self.color[name] = [int(scolor[0]*instructions[0]), int(scolor[1]*instructions[1]), int(scolor[2]*instructions[2])]
@@ -20,88 +18,117 @@ class Color():
         for name, inst in self.colmanual.items:
             self.color[name] = [max(min(255, int(inst[1][0]*inst[0][0])), 0), max(min(255, int(inst[1][1]*inst[0][1])), 0), max(min(255, int(inst[1][2]*inst[0][2])), 0)]
         
-colorb = Color([11,201,56])
+colorb = Color([125,125,125])
 cmult = {
     'wall': (0.5,0.5,0.5),
-    'empty': (1.5,1.5,1.5)
+    'empty': (1.5,1.5,1.5),
+    'walp': (0,0,0),
+    'empl': (2,2,2)
 }
 for name,c in cmult.items():
     colorb.manual_edit(c, name, colorb.color['background'])
 
 class Player():
-    def __init__(self, x, y, w, h, color, mov_dis):
+    def __init__(self, x, y, w, h, color, grid):
         self.player = {
-            'x': x,
-            'y': y,
+            'row': x,
+            'column': y,
             'width': w,
             'height': h,
             'color': color,
-            'mov_dis': mov_dis
         }
+        self.grid = grid
+        self.grid.array[self.player['row']][self.player['column']].block_stats['has_player'] = 1
+    def draw_player(self):
+        arcade.draw_rectangle_filled(self.grid.grid_stats['stx']+self.grid.grid_stats['sqwidth']*self.player['column'], 
+                                    self.grid.grid_stats['sty']+self.grid.grid_stats['sqheight']*self.player['row'], 
+                                    self.player['width'], self.player['height'], self.player['color'])
     def move(self):
-            ch = choice(['x-1', 'x1', 'y-1', 'y+1'])
-            self.player[ch[0]] = self.player[ch[0]] + int(ch[1:])*self.player['mov_dis']
-            if self.player[ch[0]] < 0:
-                self.player[ch[0]] += self.player['mov_dis']
-            elif ch[0] == 'x' and self.player[ch[0]] > SCREEN_WIDTH:
-                self.player[ch[0]] -= self.player['mov_dis']
-            elif ch[0] == 'y' and self.player[ch[0]] > SCREEN_HEIGHT:
-                self.player[ch[0]] -= self.player['mov_dis']
+        blocks = [
+            self.grid.array[min(self.player['row']+1, self.grid.grid_stats['rows']-1)][self.player['column']],
+            self.grid.array[max(self.player['row']-1, 0)][self.player['column']],
+            self.grid.array[self.player['row']][min(self.player['column']+1, self.grid.grid_stats['columns']-1)],
+            self.grid.array[self.player['row']][max(self.player['column']-1, 0)]]
+        moves = []
+        for b in blocks:
+            if b.block_stats['passable'] == True:
+                if not (b.block_stats['x'] == self.player['column'] and b.block_stats['y'] == self.player['row']):
+                    moves.append(b)
+        if len(blocks) != 0:
+            move = choice(moves)
+            self.grid.array[self.player['row']][self.player['column']].block_stats['has_player'] = 0
+            self.grid.array[self.player['row']][self.player['column']].color_definer()
+            self.player['row'] = move.block_stats['y']
+            self.player['column'] = move.block_stats['x']
+            self.grid.array[self.player['row']][self.player['column']].block_stats['has_player'] = 1
+            self.grid.array[self.player['row']][self.player['column']].color_definer()
 
 class Grid():
-    def __init__(self, hsq, vsq):
+    def __init__(self, rows, columns, sqwidth, sqheight):
         self.grid_stats = {
-            'hsq': hsq,
-            'vsq': vsq
+            'rows': rows,
+            'columns': columns,
+            'sqwidth': sqwidth,
+            'sqheight': sqheight,
+            'stx': SCREEN_WIDTH//2-sqwidth*rows//2,
+            'sty': SCREEN_HEIGHT//2-sqheight*columns//2
         }
         self.array = []
     def build_array(self):
-        for i in range(self.grid_stats['hsq']):
+        for i in range(self.grid_stats['rows']):
             row = []
-            for j in range(self.grid_stats['vsq']):
-                row.append(Block(int(randint(0,9))))
+            for j in range(self.grid_stats['columns']):
+                row.append(Block(j, i, choice([True, False])))
                 row[-1].color_definer()
             self.array.append(row)
-    def grid_draw(self, x, y, sqwidth, sqheight, linesize=1):
-        for i in range(self.grid_stats['vsq']):
-            for j in range(self.grid_stats['hsq']):
-                arcade.draw_rectangle_filled(x+(sqwidth*j), y+(sqheight*i), sqwidth-linesize, sqwidth-linesize, self.array[j][i].block_stats['color'])
+    def grid_draw(self, linesize=1):
+        for i in range(self.grid_stats['rows']):
+            for j in range(self.grid_stats['columns']):
+                arcade.draw_rectangle_filled(self.grid_stats['stx']+(self.grid_stats['sqheight']*j), self.grid_stats['sty']+(self.grid_stats['sqwidth']*i), self.grid_stats['sqwidth']-linesize, self.grid_stats['sqheight']-linesize, self.array[i][j].block_stats['color'])
 
 class Block():
-    def __init__(self, passable):
+    def __init__(self, x, y, passable):
         self.block_stats = {
-            'passable': passable
+            'x': x, 
+            'y': y,
+            'passable': passable,
+            'has_player': 0
         }
     def color_definer(self):
         global colorb
         if not self.block_stats['passable']:
-            self.block_stats['color'] = colorb.color['wall']
+            if not self.block_stats['has_player']:
+                self.block_stats['color'] = colorb.color['wall']
+            else:
+                self.block_stats['color'] = colorb.color['walp']
         else:
-            self.block_stats['color'] = colorb.color['empty']
-
-
+            if not self.block_stats['has_player']:
+                self.block_stats['color'] = colorb.color['empty']
+            else:
+                self.block_stats['color'] = colorb.color['empl']
+            
 
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(colorb.color['background'])
-        self.player = Player(80, 80, 20, 20, (22,222,22), 40)
-        self.grid = Grid(40,20)
+        self.grid = Grid(10, 5, 40, 40)
+        self.grid.build_array()
+        self.player = Player(randint(0,self.grid.grid_stats['rows']-1), randint(0,self.grid.grid_stats['columns']-1), 20, 20, (22,222,22), self.grid)
         self.delta_stockpile = 0.0
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
         # Create your sprites and sprite lists here
-        self.grid.build_array()
     def on_draw(self):
         self.clear()
     def on_update(self, delta_time):
         global delta_stockpile
         delta_stockpile += delta_time
-        if delta_stockpile > 0.5:
+        if delta_stockpile > 1:
             self.player.move()
             arcade.start_render()
-            self.grid.grid_draw(0, 0, 40, 40, 2)
-            arcade.draw_rectangle_filled(self.player.player['x'], self.player.player['y'], self.player.player['width'], self.player.player['height'], self.player.player['color'])
+            self.grid.grid_draw(2)
+            self.player.draw_player()            
             arcade.finish_render()
             delta_stockpile = 0.0
     def on_key_release(self, key, modifiers):
@@ -111,7 +138,6 @@ def main():
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
