9a9b026e8c1d43d8832ebdc2f8970999336846a0
Timkador <104220939+Timkador@users.noreply.github.com>
2022-04-30 17:41:24 +0700

    Add files via upload

--- maingame.py
+++ maingame.py
@@ -13,60 +13,71 @@ class Color():
             'background': background
         }
         self.colmanual = {}
-    def manual_edit(self, instructions, name, scolor):
-        self.colmanual[name] = [instructions, scolor]
-        self.color[name] = [int(self.color[scolor][0]*instructions[0]), int(self.color[scolor][1]*instructions[1]), int(self.color[scolor][2]*instructions[2])]
+    def manual_edit(self, inst, name, scolor):
+        self.colmanual[name] = [[inst[0], inst[1], inst[2]], scolor]
+        if len(inst) == 4:
+            self.colmanual[name] = [self.colmanual[name][0], self.colmanual[name][1], inst[3]]
+        print(self.colmanual[name])
     def color_change(self):
         for name, inst in self.colmanual.items():
-            self.color[name] = [max(min(255, int(self.color[inst[1]][0]*inst[0][0])), 0), max(min(255, int(self.color[inst[1]][1]*inst[0][1])), 0), max(min(255, int(self.color[inst[1]][2]*inst[0][2])), 0)]
+            if len(inst) == 3:
+                if inst[2] == 'f':
+                    self.flip_color(name, inst)
+            else:
+                self.color[name] = [max(min(255, int(self.color[inst[1]][0]*inst[0][0])), 0), max(min(255, int(self.color[inst[1]][1]*inst[0][1])), 0), max(min(255, int(self.color[inst[1]][2]*inst[0][2])), 0)]
     def random_color(self, name):
-        self.color[name] = [randint(0, 255), randint(0, 255), randint(0, 255)]
+        self.color[name] = [randint(66, 200), randint(66, 200), randint(66, 200)]
         colorb.color_change()
+    def flip_color(self, name, inst):
+        self.color[name] = [255-max(min(255, int(self.color[inst[1]][0]*inst[0][0])), 0), 255-max(min(255, int(self.color[inst[1]][1]*inst[0][1])), 0), 255-max(min(255, int(self.color[inst[1]][2]*inst[0][2])), 0)]
+
 
 colorb = Color([125,125,125])
 cmult = {
-    'wall': (0.5,0.5,0.5),
+    'wall': (.5,.5,.5),
     'empty': (1.5,1.5,1.5),
     'walp': (0,0,0),
-    'empl': (2,2,2)
+    'empl': (1.25,1.25,1.25, 'f'),
+    'player': (1,1,1,'f')
 }
 for name,c in cmult.items():
     colorb.manual_edit(c, name, 'background')
+colorb.color_change()
 
-class Player():
-    def __init__(self, x, y, w, h, color, grid):
-        self.player = {
+class Entity():
+    def __init__(self, x, y, w, h, colorname, grid):
+        self.entity = {
             'row': x,
             'column': y,
             'width': w,
             'height': h,
-            'color': color,
+            'color': colorname,
         }
         self.grid = grid
-        self.grid.array[self.player['row']][self.player['column']].block_stats['has_player'] = 1
-    def draw_player(self):
-        arcade.draw_rectangle_filled(self.grid.grid_stats['stx']+self.grid.grid_stats['sqwidth']*self.player['column'], 
-                                    self.grid.grid_stats['sty']+self.grid.grid_stats['sqheight']*self.player['row'], 
-                                    self.player['width'], self.player['height'], self.player['color'])
+        self.grid.array[self.entity['row']][self.entity['column']].block_stats['has_player'] = 1
+    def draw_entity(self):
+        arcade.draw_rectangle_filled(self.grid.grid_stats['stx']+self.grid.grid_stats['sqwidth']*self.entity['column'], 
+                                    self.grid.grid_stats['sty']+self.grid.grid_stats['sqheight']*self.entity['row'], 
+                                    self.entity['width'], self.entity['height'], colorb.color[self.entity['color']])
     def move(self):
         blocks = [
-            self.grid.array[min(self.player['row']+1, self.grid.grid_stats['rows']-1)][self.player['column']],
-            self.grid.array[max(self.player['row']-1, 0)][self.player['column']],
-            self.grid.array[self.player['row']][min(self.player['column']+1, self.grid.grid_stats['columns']-1)],
-            self.grid.array[self.player['row']][max(self.player['column']-1, 0)]]
+            self.grid.array[min(self.entity['row']+1, self.grid.grid_stats['rows']-1)][self.entity['column']],
+            self.grid.array[max(self.entity['row']-1, 0)][self.entity['column']],
+            self.grid.array[self.entity['row']][min(self.entity['column']+1, self.grid.grid_stats['columns']-1)],
+            self.grid.array[self.entity['row']][max(self.entity['column']-1, 0)]]
         moves = []
         for b in blocks:
             if b.block_stats['passable'] == True:
-                if not (b.block_stats['x'] == self.player['column'] and b.block_stats['y'] == self.player['row']):
+                if not (b.block_stats['x'] == self.entity['column'] and b.block_stats['y'] == self.entity['row']):
                     moves.append(b)
         if len(moves) != 0:
             move = choice(moves)
-            self.grid.array[self.player['row']][self.player['column']].block_stats['has_player'] = 0
-            self.grid.array[self.player['row']][self.player['column']].color_definer()
-            self.player['row'] = move.block_stats['y']
-            self.player['column'] = move.block_stats['x']
-            self.grid.array[self.player['row']][self.player['column']].block_stats['has_player'] = 1
-            self.grid.array[self.player['row']][self.player['column']].color_definer()
+            self.grid.array[self.entity['row']][self.entity['column']].block_stats['has_player'] = 0
+            self.grid.array[self.entity['row']][self.entity['column']].color_definer()
+            self.entity['row'] = move.block_stats['y']
+            self.entity['column'] = move.block_stats['x']
+            self.grid.array[self.entity['row']][self.entity['column']].block_stats['has_player'] = 1
+            self.grid.array[self.entity['row']][self.entity['column']].color_definer()
 
 class Grid():
     def __init__(self, rows, columns, sqwidth, sqheight):
@@ -89,7 +100,7 @@ class Grid():
     def grid_draw(self, linesize=1):
         for i in range(self.grid_stats['rows']):
             for j in range(self.grid_stats['columns']):
-                arcade.draw_rectangle_filled(self.grid_stats['stx']+(self.grid_stats['sqheight']*j), self.grid_stats['sty']+(self.grid_stats['sqwidth']*i), self.grid_stats['sqwidth']-linesize, self.grid_stats['sqheight']-linesize, self.array[i][j].block_stats['color'])
+                arcade.draw_rectangle_filled(self.grid_stats['stx']+(self.grid_stats['sqwidth']*j), self.grid_stats['sty']+(self.grid_stats['sqheight']*i), self.grid_stats['sqwidth']-linesize, self.grid_stats['sqheight']-linesize, self.array[i][j].block_stats['color'])
     def update_color(self):
         for i in self.array:
             for j in i:
@@ -101,7 +112,6 @@ class Grid():
             y_list.append(abs(self.grid_stats['sty']+(self.array[i][0].block_stats['y'])*self.grid_stats['sqheight']-y))
         for i in range(len(self.array[0])):
             x_list.append(abs(self.grid_stats['stx']+(self.array[0][i].block_stats['x'])*self.grid_stats['sqwidth']-x))
-        print([x_list.index(min(x_list)), y_list.index(min(y_list))])
         return [x_list.index(min(x_list)), y_list.index(min(y_list))]
         
 
@@ -130,9 +140,10 @@ class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(colorb.color['background'])
-        self.grid = Grid(10, 5, 40, 40)
+        self.grid = Grid(10, 10, 40, 40)
         self.grid.build_array()
-        self.player = Player(randint(0,self.grid.grid_stats['rows']-1), randint(0,self.grid.grid_stats['columns']-1), 20, 20, (22,222,22), self.grid)
+        self.player = Entity(randint(0,self.grid.grid_stats['rows']-1), randint(0,self.grid.grid_stats['columns']-1), 20, 20, 'player', self.grid)
+        self.entity_list = [self.player]
         self.move_frequence = 0.0
     def setup(self):
         """ Set up the game variables. Call to re-start the game. """
@@ -146,8 +157,8 @@ class MyGame(arcade.Window):
             self.player.move()
             move_frequence = 0.0
         arcade.start_render()
-        self.grid.grid_draw(2)
-        self.player.draw_player()            
+        self.grid.grid_draw(0)
+        self.player.draw_entity()            
         arcade.finish_render()
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.C:
